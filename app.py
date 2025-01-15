88b9ca8d880fe54c45f6349c55f3bce60c32259f
Mar <mariana.ramos.caceres@gmail.com>
2025-01-14 19:16:57 -0600

    codigo para histograma agregado

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ import streamlit as st
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
+if hist_button:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
