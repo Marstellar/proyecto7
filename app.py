29fd20490953eef8aae9c086c3cac4447daa11e4
Mar <mariana.ramos.caceres@gmail.com>
2025-01-15 11:39:02 -0600

    contenido de app agregado

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ import streamlit as st
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
+st.header('Comparativo de marcas de Auto')
 if hist_button:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
+
+disp_button = st.button('Construir grafico de dispersion')
+
+if disp_button:
+    st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)    
